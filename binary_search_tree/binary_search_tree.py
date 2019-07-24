ea16884fefbd44b6f7df1dc5d1fcbfd887364ac5
Francis Chen <fncis.chen@gmail.com>
2019-07-24 11:57:12 -0700

    finished BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -32,14 +32,14 @@ class BinarySearchTree:
     # check for three conditions
 
     # check if the target equals self value
-    if target == self.value  
+    if target == self.value:  
     # if so, return True
       return True
     
     # if not, check and see if the target is less than or equal to the target
     if target < self.value:
       # if so, search in the left subtree
-      if self.left not None:
+      if self.left != None:
         self.left.contains(target)
       # if the left subtree does not exist return False
       else:
@@ -47,7 +47,7 @@ class BinarySearchTree:
     
     else:
       # if not, search on the right subtree
-      if self.right not None:
+      if self.right != None:
         self.right.contains(target)
       # if the right subtree does not exist, return False 
       else:
@@ -63,16 +63,54 @@ class BinarySearchTree:
     # traversing the right tree until there is no right node 
       while right:
         max = right.value 
-        right = right.self.right 
+        right = right.right 
 
       return max 
 
-    pass
 
+# https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
+# depth first or breadth search 
   def for_each(self, cb):
-    pass
 
-# 
+    # breadth traversal
+    root = self.value
+    left = self.left 
+    right = self.right 
+    
+    # set up an iterator of levels to loop through 
+    levels = []
 
-# 8
-# 3 10 (left is always smaller than right)
+    # apply callback function to root
+    cb(root) 
+
+    # assume no empty levels 
+    noEmptyLevels = False 
+
+    while noEmptyLevels:
+      noEmptyLevels = True 
+    
+    # add BST subtrees on the levels so that we can apply 
+      levels.append(left)
+      levels.append(right)
+    # the callback function on the values of each BST
+
+      for level in levels:
+        cb(level.root)
+      
+    # replace the BST subtrees with new subtrees by looping through
+    # the levels subtree
+
+      oldlevels = levels
+      levels = []
+      for level in oldlevels:
+        levels.append(level.left)
+        levels.append(level.right)
+      
+    # what condition to check -? if ALL the level BST left and right are equal to None - let the while loop know
+      for level in levels:
+          if level != None:
+            noEmptyLevels = False
+            break 
+    
+    # else: 
+    # continue this sequence until the tree has no more roots to loop through
